9e48942cad10de8ae8d9afefecccfdabfbaf66be
Xavier Gouchet <xavier.gouchet@gmail.com>
2020-12-18 10:34:05 +0100

    Update dependencies

--- lib/typescript/src/index.d.ts
+++ lib/typescript/src/index.d.ts
@@ -1,3 +1,6 @@
-import { DdLogsType, DdLogsConfiguration } from './types';
+import { DdSdkConfiguration, DdSdkType, DdLogsType, DdTraceType, DdRumType } from './types';
+declare const DdSdk: DdSdkType;
 declare const DdLogs: DdLogsType;
-export { DdLogs, DdLogsConfiguration };
+declare const DdTrace: DdTraceType;
+declare const DdRum: DdRumType;
+export { DdSdkConfiguration, DdSdk, DdLogs, DdTrace, DdRum };
