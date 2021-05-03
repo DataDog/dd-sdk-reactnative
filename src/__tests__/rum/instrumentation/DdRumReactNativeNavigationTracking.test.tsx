c5177b01b54ddb5069b3abba658f766c05322e65
Xavier Gouchet <xavier.gouchet@gmail.com>
2021-05-03 13:22:05 +0200

    RUMM-1297 Track navigation from wix library

---
+++ src/__tests__/rum/instrumentation/DdRumReactNativeNavigationTracking.test.tsx
@@ -0,0 +1,111 @@
+/*
+ * Unless explicitly stated otherwise all files in this repository are licensed under the Apache License Version 2.0.
+ * This product includes software developed at Datadog (https://www.datadoghq.com/).
+ * Copyright 2016-Present Datadog, Inc.
+ */
+
+import React from 'react';
+import { View, Text, Button, AppState } from 'react-native';
+import { render, fireEvent } from '@testing-library/react-native';
+import { DdRum } from '../../../index';
+import DdRumReactNativeNavigationTracking from '../../../rum/instrumentation/DdRumReactNativeNavigationTracking';
+import { Navigation } from 'react-native-navigation';
+
+jest.mock('../../../foundation', () => {
+    return {
+        DdRum: {
+            // eslint-disable-next-line @typescript-eslint/no-empty-function
+            startView: jest.fn().mockImplementation(() => { }),
+            stopView: jest.fn().mockImplementation(() => { })
+        },
+    };
+});
+jest.useFakeTimers();
+
+let mockRegisterComponentListener = jest.fn().mockImplementation(() => { })
+jest.mock('react-native-navigation', () => {
+    return {
+        Navigation: {
+            events: jest.fn().mockImplementation(() => { 
+                return {
+                    registerComponentListener: mockRegisterComponentListener
+                }
+            })
+        }
+    }
+});
+
+
+let originalCreateMethod: Function
+
+beforeEach(() => {
+    
+    jest.setTimeout(20000);
+    DdRum.startView.mockClear();
+    DdRum.stopView.mockClear();
+    
+    DdRumReactNativeNavigationTracking['trackedComponentIds'] = [];
+    DdRumReactNativeNavigationTracking['isTracking'] = false;
+    originalCreateMethod = React.createElement
+})
+
+afterEach(() => {
+    React.createElement = originalCreateMethod
+})
+
+// Unit tests
+
+
+
+it('M register only once W startTracking()', async () => {
+
+    // GIVEN
+    let componentId = "component42"
+    DdRumReactNativeNavigationTracking.startTracking();
+
+    // WHEN
+    const testInstance = React.createElement('View', { 'componentId': componentId});
+    const otherTestInstance = React.createElement('View', { 'componentId': componentId, 'something': 'else'});
+    const listener = mockRegisterComponentListener.mock.calls[0][0];
+
+    // THEN
+    expect(mockRegisterComponentListener.mock.calls.length).toBe(1);
+})
+
+
+it('M send a RUM ViewEvent W startTracking() componentDidAppear', async () => {
+
+    // GIVEN
+    let componentId = "component42"
+    DdRumReactNativeNavigationTracking.startTracking();
+
+    // WHEN
+    const testInstance = React.createElement('View', { 'componentId': componentId});
+    const listener = mockRegisterComponentListener.mock.calls[0][0];
+    listener.componentDidAppear();
+
+    // THEN
+    expect(DdRum.startView.mock.calls.length).toBe(1);
+    expect(DdRum.startView.mock.calls[0][0]).toBe(componentId);
+    expect(DdRum.startView.mock.calls[0][1]).toBe(componentId);
+    expect(DdRum.startView.mock.calls[0][3]).toStrictEqual({});
+})
+
+
+
+it('M send a RUM ViewEvent W startTracking() componentDidDisappear', async () => {
+
+    // GIVEN
+    let componentId = "component42"
+    DdRumReactNativeNavigationTracking.startTracking();
+
+    // WHEN
+    const testInstance = React.createElement('View', { 'componentId': componentId});
+    const listener = mockRegisterComponentListener.mock.calls[0][0];
+    listener.componentDidDisappear();
+
+    // THEN
+    expect(DdRum.stopView.mock.calls.length).toBe(1);
+    expect(DdRum.stopView.mock.calls[0][0]).toBe(componentId);
+    expect(DdRum.stopView.mock.calls[0][2]).toStrictEqual({});
+})
---
+++ src/rum/instrumentation/DdRumReactNativeNavigationTracking.tsx
@@ -0,0 +1,53 @@
+/*
+ * Unless explicitly stated otherwise all files in this repository are licensed under the Apache License Version 2.0.
+ * This product includes software developed at Datadog (https://www.datadoghq.com/).
+ * Copyright 2016-Present Datadog, Inc.
+ */
+
+import React from 'react'
+import { Navigation } from 'react-native-navigation';
+import { DdRum } from '../../foundation';
+
+/**
+* Provides RUM integration for the [React Native Navigation](https://wix.github.io/react-native-navigation) API.
+*/
+export default class DdRumReactNativeNavigationTracking {
+
+    private static isTracking = false
+    private static trackedComponentIds : Array<any> = [];
+
+    /**
+     * Starts tracking the Navigation and sends a RUM View event every time a root View component appear/disappear.
+     */
+    static startTracking(): void {
+        // extra safety to avoid wrapping more than 1 time this function
+        if (DdRumReactNativeNavigationTracking.isTracking) {
+            return
+        }
+        const original = React.createElement
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+        React.createElement = (element: any, props: any, ...children: any): any => {
+            if (
+                props.componentId != undefined 
+                && !DdRumReactNativeNavigationTracking.trackedComponentIds.includes(props.componentId)
+            ) {
+                const componentId = props.componentId
+                Navigation.events().registerComponentListener(
+                    {
+                        componentDidAppear: () => {
+                            DdRum.startView(componentId, componentId, new Date().getTime(), {});
+                        },
+                        componentDidDisappear: () => {
+                            DdRum.stopView(componentId, new Date().getTime(), {});
+                        },
+                    },
+                    componentId 
+                );
+                DdRumReactNativeNavigationTracking.trackedComponentIds.push(componentId);
+            }
+
+            return original(element, props, ...children)
+        }
+        DdRumReactNativeNavigationTracking.isTracking = true
+    }
+};
