39361a42515e80e43f3569059f69730a92baefab
Louis Zawadzki <louis.zawadzki@datadoghq.com>
2022-05-05 15:58:35 +0200

    RUMM-1891 Make memoization work with onPress

---
+++ packages/core/src/rum/instrumentation/ShallowObjectEqualityChecker.ts
@@ -0,0 +1,17 @@
+/*
+ * Unless explicitly stated otherwise all files in this repository are licensed under the Apache License Version 2.0.
+ * This product includes software developed at Datadog (https://www.datadoghq.com/).
+ * Copyright 2016-Present Datadog, Inc.
+ */
+
+/**
+ * Does a shallow comparison between 2 objects
+ */
+export const areObjectShallowEqual = (objectA: Record<string, unknown>, objectB: Record<string, unknown>): boolean => {
+    const keys = Object.keys(objectA);
+    if (keys.length !== Object.keys(objectB).length) return false
+    for (const key of keys) {
+        if (objectA[key] !== objectB[key]) return false
+    }
+    return true;
+}
