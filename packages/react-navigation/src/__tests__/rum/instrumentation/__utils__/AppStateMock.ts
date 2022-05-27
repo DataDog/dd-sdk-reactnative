1c5aded7cde66e1e45fe612f2bd9fd9ec29a8c46
louiszawadzki <louis.zawadzki@datadoghq.com>
2022-05-27 14:09:39 +0200

    RUMM-2190 Support new AppState interface

---
+++ packages/react-navigation/src/__tests__/rum/instrumentation/__utils__/AppStateMock.ts
@@ -0,0 +1,75 @@
+/*
+ * Unless explicitly stated otherwise all files in this repository are licensed under the Apache License Version 2.0.
+ * This product includes software developed at Datadog (https://www.datadoghq.com/).
+ * Copyright 2016-Present Datadog, Inc.
+ */
+
+import type {
+    AppStateEvent,
+    AppStateStatus,
+    NativeEventSubscription
+} from 'react-native';
+
+type handler = (type: AppStateStatus) => void;
+
+export class AppStateMock {
+    private handlers: {
+        [eventType: string]: {
+            callback: handler;
+            subscription: NativeEventSubscription;
+        }[];
+    } = {};
+
+    addEventListener = (type: AppStateEvent, callback: handler) => {
+        if (!this.handlers[type]) {
+            this.handlers[type] = [];
+        }
+        const subscription = {
+            remove: () => this.removeListeners(type, callback)
+        };
+        this.handlers[type].push({
+            callback,
+            subscription
+        });
+
+        return subscription;
+    };
+
+    private removeListeners = (type: string, callback: handler) => {
+        if (!this.handlers[type]) {
+            return;
+        }
+        const callbackIndex = this.handlers[type].findIndex(
+            handler => handler.callback === callback
+        );
+        if (callbackIndex === -1) {
+            return;
+        }
+        this.handlers[type] = [
+            ...this.handlers[type].slice(0, callbackIndex),
+            ...this.handlers[type].slice(
+                callbackIndex + 1,
+                this.handlers[type].length
+            )
+        ];
+    };
+
+    changeValue = (value: AppStateStatus) => {
+        if (!this.handlers.change) {
+            return;
+        }
+        this.handlers.change.forEach(handler => {
+            try {
+                handler.callback(value);
+            } catch (e) {
+                console.warn(
+                    `Failure while executing callback for value ${value}`
+                );
+            }
+        });
+    };
+
+    removeAllListeners = () => {
+        this.handlers = {};
+    };
+}
