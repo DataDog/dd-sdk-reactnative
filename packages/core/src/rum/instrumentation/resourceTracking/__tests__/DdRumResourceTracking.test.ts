e771746701301a6be4fcd7c50bb2cececa1ddccc
louiszawadzki <louis.zawadzki@datadoghq.com>
2022-06-02 13:28:12 +0200

    RUMM-2216 Add test for xhr proxy teardown

---
+++ packages/core/src/rum/instrumentation/resourceTracking/__tests__/DdRumResourceTracking.test.ts
@@ -0,0 +1,62 @@
+/*
+ * Unless explicitly stated otherwise all files in this repository are licensed under the Apache License Version 2.0.
+ * This product includes software developed at Datadog (https://www.datadoghq.com/).
+ * Copyright 2016-Present Datadog, Inc.
+ */
+
+import { NativeModules } from 'react-native';
+
+import { DdRumResourceTracking } from '../DdRumResourceTracking';
+
+import { XMLHttpRequestMock } from './__utils__/XMLHttpRequestMock';
+
+const DdRum = NativeModules.DdRum;
+
+const flushPromises = () => new Promise(setImmediate);
+
+beforeEach(() => {
+    DdRum.startResource.mockClear();
+    DdRum.stopResource.mockClear();
+    global.XMLHttpRequest = XMLHttpRequestMock;
+});
+
+afterEach(() => {
+    global.XMLHttpRequest = undefined;
+});
+
+const executeRequest = () => {
+    const xhr = new XMLHttpRequestMock();
+    xhr.open('GET', 'https://api.example.com/v2/user');
+    xhr.send();
+    xhr.notifyResponseArrived();
+    xhr.complete(200, 'ok');
+};
+
+describe('DdRumResourceTracking', () => {
+    it('removes all side effects when tracking is stopped', async () => {
+        // GIVEN
+        global.XMLHttpRequest = XMLHttpRequestMock;
+        DdRumResourceTracking.startTracking({
+            tracingSamplingRate: 100,
+            firstPartyHosts: ['example.com']
+        });
+
+        // WHEN
+        executeRequest();
+        await flushPromises();
+
+        // THEN
+        expect(DdRum.startResource).toHaveBeenCalledTimes(1);
+        expect(DdRum.stopResource).toHaveBeenCalledTimes(1);
+
+        // WHEN
+        DdRum.startResource.mockClear();
+        DdRum.stopResource.mockClear();
+        DdRumResourceTracking.stopTracking();
+        executeRequest();
+
+        // THEN
+        expect(DdRum.startResource).toHaveBeenCalledTimes(0);
+        expect(DdRum.stopResource).toHaveBeenCalledTimes(0);
+    });
+});
