34bc0d2de4a5c35e46aa7a625e35caab8c7f8d14
louiszawadzki <louis.zawadzki@datadoghq.com>
2022-06-14 15:44:20 +0200

    Add header to jest setup file

--- jest.setup.js
+++ jest.setup.js
@@ -1,3 +1,9 @@
+/*
+ * Unless explicitly stated otherwise all files in this repository are licensed under the Apache License Version 2.0.
+ * This product includes software developed at Datadog (https://www.datadoghq.com/).
+ * Copyright 2016-Present Datadog, Inc.
+ */
+
 /**
  * In the tests, performance.now sometimes does not change as fast as Date.now.
  * This results in short intervals sometimes having a duration of 0ms and flaky tests.
