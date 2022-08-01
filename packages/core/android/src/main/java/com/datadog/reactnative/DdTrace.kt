45a58561911809202077a11994a0e7d18de158b5
louiszawadzki <louis.zawadzki@datadoghq.com>
2022-08-01 15:24:36 +0200

    RUMM-2085 Migrate DdTrace android

--- packages/core/android/src/main/java/com/datadog/reactnative/DdTrace.kt
+++ packages/core/android/src/main/java/com/datadog/reactnative/DdTrace.kt
@@ -6,21 +6,28 @@
 
 package com.datadog.reactnative
 
-import com.datadog.android.bridge.DdBridge
-import com.datadog.android.bridge.DdTrace as SDKDdTrace
+import com.datadog.android.tracing.AndroidTracer
 import com.facebook.react.bridge.Promise
 import com.facebook.react.bridge.ReactApplicationContext
 import com.facebook.react.bridge.ReactContextBaseJavaModule
 import com.facebook.react.bridge.ReactMethod
 import com.facebook.react.bridge.ReadableArray
 import com.facebook.react.bridge.ReadableMap
+import io.opentracing.Span
+import io.opentracing.Tracer
+import java.util.concurrent.TimeUnit
+
 
 /**
  * The entry point to use Datadog's Trace feature.
  */
-class DdTrace(reactContext: ReactApplicationContext) : ReactContextBaseJavaModule(reactContext) {
+class DdTrace(reactContext: ReactApplicationContext, private val tracerProvider: () -> Tracer = { AndroidTracer.Builder().build() }) : ReactContextBaseJavaModule(reactContext) {
 
-    private val nativeInstance: SDKDdTrace = DdBridge.getDdTrace(reactContext)
+    private val spanMap: MutableMap<String, Span> = mutableMapOf()
+
+    // lazy here is on purpose. The thing is that this class will be instantiated even
+    // before Sdk.initialize is called, but Tracer can be created only after SDK is initialized.
+    private val tracer by lazy { tracerProvider.invoke() }
 
     override fun getName(): String = "DdTrace"
 
@@ -32,8 +39,17 @@ class DdTrace(reactContext: ReactApplicationContext) : ReactContextBaseJavaModul
      */
     @ReactMethod
     fun startSpan(operation: String, context: ReadableMap, timestampMs: Double, promise: Promise) {
-        val result = nativeInstance.startSpan(operation, context.toHashMap(), timestampMs.toLong())
-        promise.resolve(result)
+        val span = tracer.buildSpan(operation)
+            .withStartTimestamp(TimeUnit.MILLISECONDS.toMicros(timestampMs.toLong()))
+            .start()
+        val spanContext = span.context()
+
+        span.setTags(context.toHashMap())
+        span.setTags(GlobalState.globalAttributes)
+        val spanId = spanContext.toSpanId()
+        spanMap[spanId] = span
+
+        promise.resolve(spanId)
     }
 
     /**
@@ -44,8 +60,26 @@ class DdTrace(reactContext: ReactApplicationContext) : ReactContextBaseJavaModul
      */
     @ReactMethod
     fun finishSpan(spanId: String, context: ReadableMap, timestampMs: Double, promise: Promise) {
-        nativeInstance.finishSpan(spanId, context.toHashMap(), timestampMs.toLong())
+        val span = spanMap.remove(spanId)
+        if (span == null) {
+            promise.resolve(null)
+            return
+        }
+        span.setTags(context.toHashMap())
+        span.setTags(GlobalState.globalAttributes)
+        span.finish(TimeUnit.MILLISECONDS.toMicros(timestampMs.toLong()))
+
         promise.resolve(null)
     }
 
+    private fun Span.setTags(tags: Map<String, Any?>) {
+        tags.forEach { (key, value) ->
+            when (value) {
+                is Boolean -> setTag(key, value)
+                is Number -> setTag(key, value)
+                is String -> setTag(key, value)
+                else -> setTag(key, value?.toString())
+            }
+        }
+    }
 }
