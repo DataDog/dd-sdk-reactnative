516281bc64edc9a437717469e095fd631abf6e00
louiszawadzki <louis.zawadzki@datadoghq.com>
2022-08-01 11:52:40 +0200

    RUMM-2085 Migrate DdTrace iOS

--- packages/core/ios/Sources/DdTrace.swift
+++ packages/core/ios/Sources/DdTrace.swift
@@ -5,7 +5,7 @@
  */
 
 import Foundation
-import DatadogSDKBridge
+import Datadog
 
 @objc(DdTrace)
 class RNDdTrace: NSObject {
@@ -15,21 +15,56 @@ class RNDdTrace: NSObject {
         return false
     }
 
-    let nativeInstance: DdTrace = Bridge.getDdTrace()
+    private lazy var tracer: OTTracer = tracerProvider()
+    private let tracerProvider: () -> OTTracer
+    private(set) var spanDictionary = [NSString: OTSpan]()
 
+    internal init(_ tracerProvider: @escaping () -> OTTracer) {
+        self.tracerProvider = tracerProvider
+    }
+
+    override convenience init() {
+        self.init { Tracer.initialize(configuration: Tracer.Configuration()) }
+    }
     @objc(methodQueue)
     let methodQueue: DispatchQueue = sharedQueue
 
     @objc(startSpan:withContext:withTimestampms:withResolver:withRejecter:)
     func startSpan(operation: NSString, context: NSDictionary, timestampMs: Int64, resolve:RCTPromiseResolveBlock, reject:RCTPromiseRejectBlock) -> Void {
-        let result = nativeInstance.startSpan(operation: operation, context: context, timestampMs: timestampMs)
-        resolve(result)
+        let id = UUID().uuidString as NSString
+        let timeIntervalSince1970: TimeInterval = Double(timestampMs) / 1_000
+        let startDate = Date(timeIntervalSince1970: timeIntervalSince1970)
+
+        objc_sync_enter(self)
+        spanDictionary[id] = tracer.startSpan(
+            operationName: operation as String,
+            childOf: nil,
+            tags: castAttributesToSwift(context).mergeWithGlobalAttributes(),
+            startTime: startDate
+        )
+        objc_sync_exit(self)
+
+        resolve(id)
     }
 
     @objc(finishSpan:withContext:withTimestampms:withResolver:withRejecter:)
     func finishSpan(spanId: NSString, context: NSDictionary, timestampMs: Int64, resolve:RCTPromiseResolveBlock, reject:RCTPromiseRejectBlock) -> Void {
-        nativeInstance.finishSpan(spanId: spanId, context: context, timestampMs: timestampMs)
+        objc_sync_enter(self)
+        let optionalSpan = spanDictionary.removeValue(forKey: spanId)
+        objc_sync_exit(self)
+
+        if let span = optionalSpan {
+            set(tags: castAttributesToSwift(context).mergeWithGlobalAttributes(), to: span)
+            let timeIntervalSince1970: TimeInterval = Double(timestampMs) / 1_000
+            span.finish(at: Date(timeIntervalSince1970: timeIntervalSince1970))
+        }
+        
         resolve(nil)
     }
 
+    private func set(tags: [String: Encodable], to span: OTSpan) {
+        for (key, value) in tags {
+            span.setTag(key: key, value: value)
+        }
+    }
 }
