9c9bd3930f204417797382d7445f300acd868798
louiszawadzki <louis.zawadzki@datadoghq.com>
2022-10-13 17:49:20 +0200

    Implement JS refresh rate in native

---
+++ packages/core/android/src/main/kotlin/com/datadog/reactnative/Vitals.kt
@@ -0,0 +1,160 @@
+package com.datadog.reactnative
+
+import android.util.Log
+import android.view.Choreographer
+import java.util.concurrent.TimeUnit
+import kotlin.math.max
+import kotlin.math.min
+
+internal interface VitalObserver {
+    fun onNewSample(value: Double)
+}
+
+
+/**
+ * Reads the UI framerate based on the [Choreographer.FrameCallback] and notify a [VitalObserver].
+ */
+internal class VitalFrameCallback(
+    private val observer: VitalObserver,
+    private val keepRunning: () -> Boolean
+) : Choreographer.FrameCallback {
+
+    internal var lastFrameTimestampNs: Long = 0L
+
+    // region Choreographer.FrameCallback
+
+    override fun doFrame(frameTimeNanos: Long) {
+        Log.i("DD-SDK", "in doFrame")
+        if (lastFrameTimestampNs != 0L) {
+            val durationNs = (frameTimeNanos - lastFrameTimestampNs).toDouble()
+            if (durationNs > 0.0) {
+                val frameRate = ONE_SECOND_NS / durationNs
+                if (frameRate in VALID_FPS_RANGE) {
+                    observer.onNewSample(frameRate)
+                }
+            }
+        }
+        lastFrameTimestampNs = frameTimeNanos
+
+        @Suppress("UnsafeThirdPartyFunctionCall") // internal safe call
+        if (keepRunning()) {
+            try {
+                Choreographer.getInstance().postFrameCallback(this)
+            } catch (e: IllegalStateException) {
+                Log.e("DD-SDK", "problem in vitalFrame")
+            }
+        }
+    }
+
+    // endregion
+
+    companion object {
+        val ONE_SECOND_NS: Double = TimeUnit.SECONDS.toNanos(1).toDouble()
+
+        private const val MIN_FPS: Double = 1.0
+        private const val MAX_FPS: Double = 240.0
+        val VALID_FPS_RANGE = MIN_FPS.rangeTo(MAX_FPS)
+    }
+}
+
+internal interface VitalListener {
+    fun onVitalUpdate(info: VitalInfo)
+}
+
+internal interface VitalMonitor : VitalObserver {
+    fun getLastSample(): Double
+
+    fun register(listener: VitalListener)
+
+    fun unregister(listener: VitalListener)
+}
+
+internal data class VitalInfo(
+    val sampleCount: Int,
+    val minValue: Double,
+    val maxValue: Double,
+    val meanValue: Double
+) {
+    companion object {
+        val EMPTY = VitalInfo(0, Double.MAX_VALUE, -Double.MAX_VALUE, 0.0)
+    }
+}
+
+
+
+internal class AggregatingVitalMonitor : VitalMonitor {
+
+    private var lastKnownSample: Double = Double.NaN
+
+    private val listeners: MutableMap<VitalListener, VitalInfo> = mutableMapOf()
+
+    // region VitalObserver
+
+    override fun onNewSample(value: Double) {
+        lastKnownSample = value
+        notifyListeners(value)
+    }
+
+    // endregion
+
+    // region VitalMonitor
+
+    override fun getLastSample(): Double {
+        return lastKnownSample
+    }
+
+    override fun register(listener: VitalListener) {
+        val value = lastKnownSample
+        synchronized(listeners) {
+            listeners[listener] = VitalInfo.EMPTY
+        }
+        if (!value.isNaN()) {
+            notifyListener(listener, value)
+        }
+    }
+
+    override fun unregister(listener: VitalListener) {
+        synchronized(listeners) {
+            listeners.remove(listener)
+        }
+    }
+
+    // endregion
+
+    // region Internal
+
+    private fun notifyListeners(value: Double) {
+        synchronized(listeners) {
+            for (listener in listeners.keys) {
+                notifyListener(listener, value)
+            }
+        }
+    }
+
+    private fun notifyListener(listener: VitalListener, value: Double) {
+        val vitalInfo = listeners[listener] ?: VitalInfo.EMPTY
+        val newSampleCount = vitalInfo.sampleCount + 1
+
+        // Assuming M(n) is the mean value of the first n samples
+        // M(n) = ∑ sample(n) / n
+        // n⨉M(n) = ∑ sample(n)
+        // M(n+1) = ∑ sample(n+1) / (n+1)
+        //        = [ sample(n+1) + ∑ sample(n) ] / (n+1)
+        //        = (sample(n+1) + n⨉M(n)) / (n+1)
+        val meanValue = (value + (vitalInfo.sampleCount * vitalInfo.meanValue)) / newSampleCount
+
+        val updatedInfo = VitalInfo(
+            newSampleCount,
+            min(value, vitalInfo.minValue),
+            max(value, vitalInfo.maxValue),
+            meanValue
+        )
+        listener.onVitalUpdate(updatedInfo)
+        synchronized(listeners) {
+            listeners[listener] = updatedInfo
+        }
+        Log.i("DD_FPS", value.toString())
+    }
+
+    // endregion
+}
