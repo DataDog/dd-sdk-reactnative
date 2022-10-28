3559a74871a832670954555d88419f72f9f37457
louiszawadzki <louis.zawadzki@datadoghq.com>
2022-10-28 13:48:37 +0200

    Handle Sentry with Datadog on Expo

--- expo/src/plugin/withIosSourcemaps/withIosSourcemaps.ts
+++ expo/src/plugin/withIosSourcemaps/withIosSourcemaps.ts
@@ -7,6 +7,9 @@
 import type { ConfigPlugin } from '@expo/config-plugins';
 import { withXcodeProject } from '@expo/config-plugins';
 
+const SOURCEMAP_FILE_COMMAND = 'export SOURCEMAP_FILE=./main.jsbundle.map';
+const DATADOG_XCODE_COMMAND = `yarn datadog-ci react-native xcode \`node --print \\\"require('path').dirname(require.resolve('react-native/package.json')) + '/scripts/react-native-xcode.sh'\\\"\``;
+
 const withIosSourcemaps: ConfigPlugin<void> = config => {
     return withXcodeProject(config, async config => {
         const xcodeProject = config.modResults;
@@ -17,11 +20,28 @@ const withIosSourcemaps: ConfigPlugin<void> = config => {
         if (bundlePhase.shellScript.match('datadog-ci react-native xcode')) {
             return config;
         }
+        if (bundlePhase.shellScript.match('sentry')) {
+            /**
+             * In the case where Sentry is already added, we add a new line to
+             * bundle the javascript code twice and this time send the sourcemap
+             * to Datadog.
+             * That's the safest way to make sure we don't break the script.
+             *
+             * We also need to remove the final " and add it back for the script to
+             * be well-formatted.
+             */
+            bundlePhase.shellScript = `${bundlePhase.shellScript.replace(
+                /.$/,
+                ''
+            )}\\n ${DATADOG_XCODE_COMMAND}"`;
+
+            return config;
+        }
 
         const [beforeScript, afterScript] = bundlePhase.shellScript.split(
             "`node --print \\\"require('path').dirname(require.resolve('react-native/package.json')) + '/scripts/react-native-xcode.sh'\\\"`"
         );
-        const datadogScript = `export SOURCEMAP_FILE=./main.jsbundle.map\\n yarn datadog-ci react-native xcode \`node --print \\\"require('path').dirname(require.resolve('react-native/package.json')) + '/scripts/react-native-xcode.sh'\\\"\``;
+        const datadogScript = `${SOURCEMAP_FILE_COMMAND}\\n ${DATADOG_XCODE_COMMAND}`;
         bundlePhase.shellScript = `${beforeScript}${datadogScript}${afterScript}`;
 
         return config;
