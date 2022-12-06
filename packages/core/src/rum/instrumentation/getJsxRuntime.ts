0e4d35579ee1cc9db55c5274ee6b6ad281838490
louiszawadzki <louis.zawadzki@datadoghq.com>
2022-12-06 14:47:49 +0100

    Replace dynamic require by postinstall script

--- packages/core/src/rum/instrumentation/getJsxRuntime.ts
+++ packages/core/src/rum/instrumentation/getJsxRuntime.ts
@@ -7,22 +7,22 @@
 export const getJsxRuntime = () => {
     // eslint-disable-next-line global-require, @typescript-eslint/no-var-requires
     const [major, minor] = require('react/package.json').version.split('.');
-    // We need to check on the version of React before calling require('react/jsx-runtime') as
+    // We need to check on the version of React before requiring 'react/jsx-runtime') as
     // it might crash other exports if the export does not exist (even if there is a try/catch).
     // See: https://github.com/facebook/metro/issues/836
     if (!isJsxExportedInReactVersion(major, minor)) {
         throw new Error('React version does not support new jsx transform');
     }
 
-    // React Native does some premature optimizations if the require target is a static string, by making
-    // it dynamic we prevent RN from trying to resolve react/jsx-runtime before the function is called
-    const jsxRuntimePackage = isJsxExportedInReactVersion(major, minor)
-        ? 'react/jsx-runtime'
-        : 'react';
-
-    // We have to use inline require here because older React versions (below 17) don't have jsx-runtime
-    // eslint-disable-next-line global-require, @typescript-eslint/no-var-requires, import/no-dynamic-require
-    const jsxRuntime = require(jsxRuntimePackage);
+    // /!\/!\/!\/!\/!\/!\
+    // We have to use inline require here because older React versions (below 16.14) don't have jsx-runtime.
+    // metro does not support dynamic require, so 'react/jsx-runtime' is replaced by 'react' on postinstall
+    // when react is equal or above 16.14 to avoid this require call to fail during the import phase and then
+    // disable all further imports.
+    // /!\/!\/!\/!\/!\/!\
+    //
+    // eslint-disable-next-line global-require, @typescript-eslint/no-var-requires
+    const jsxRuntime = require('react/jsx-runtime');
     if (!jsxRuntime.jsx) {
         throw new Error('React version does not support new jsx transform');
     }
