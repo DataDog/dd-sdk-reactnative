aa9fc8f43d84a47c6eabc71b8ed85002092d2963
louiszawadzki <louis.zawadzki@datadoghq.com>
2023-01-10 14:53:56 +0100

    Add missing headers

--- packages/core/src/logs/__tests__/eventMapper.test.ts
+++ packages/core/src/logs/__tests__/eventMapper.test.ts
@@ -1,3 +1,9 @@
+/*
+ * Unless explicitly stated otherwise all files in this repository are licensed under the Apache License Version 2.0.
+ * This product includes software developed at Datadog (https://www.datadoghq.com/).
+ * Copyright 2016-Present Datadog, Inc.
+ */
+
 /* eslint-disable @typescript-eslint/ban-ts-comment */
 import { DdSdk } from '../../foundation';
 import { applyLogEventMapper, formatLogEvent } from '../eventMapper';
--- packages/core/src/sdk/UserInfoSingleton/types.ts
+++ packages/core/src/sdk/UserInfoSingleton/types.ts
@@ -1,7 +1,13 @@
+/*
+ * Unless explicitly stated otherwise all files in this repository are licensed under the Apache License Version 2.0.
+ * This product includes software developed at Datadog (https://www.datadoghq.com/).
+ * Copyright 2016-Present Datadog, Inc.
+ */
+
 export type UserInfo = {
-    readonly id?: string;
-    readonly name?: string;
-    readonly email?: string;
+    id?: string;
+    name?: string;
+    email?: string;
     extraInfo?: {
         [key: string]: unknown;
     };
