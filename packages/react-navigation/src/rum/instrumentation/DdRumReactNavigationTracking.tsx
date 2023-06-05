13123d45f7fb9760f90ae75e05fcad72508decee
louiszawadzki <louis.zawadzki@datadoghq.com>
2023-06-05 15:43:29 +0200

    Remove predicate variable

--- packages/react-navigation/src/rum/instrumentation/DdRumReactNavigationTracking.tsx
+++ packages/react-navigation/src/rum/instrumentation/DdRumReactNavigationTracking.tsx
@@ -195,9 +195,10 @@ export class DdRumReactNavigationTracking {
         appStateStatus: AppStateStatus
     ) {
         const key = route.key;
-
-        const predicate = DdRumReactNavigationTracking.viewNamePredicate;
-        const screenName = predicate(route, route.name);
+        const screenName = DdRumReactNavigationTracking.viewNamePredicate(
+            route,
+            route.name
+        );
 
         if (key != null && screenName != null) {
             if (appStateStatus === 'background') {
