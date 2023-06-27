35a0c8b09da8f095cc83195b6960dd517e70f3c1
louiszawadzki <louis.zawadzki@datadoghq.com>
2023-06-27 11:47:13 +0200

    Fix pack command after yarn upgrade

--- update-release-content.sh
+++ update-release-content.sh
@@ -8,7 +8,7 @@ yarn workspace @datadog/mobile-react-native pack
 yarn workspace @datadog/mobile-react-navigation pack
 yarn workspace @datadog/mobile-react-native-navigation pack
 
-./check-release-content.sh -p packages/core/datadog-mobile-react-native-v$version.tgz > packages/core/release-content.txt
-./check-release-content.sh -p packages/react-navigation/datadog-mobile-react-navigation-v$version.tgz > packages/react-navigation/release-content.txt
-./check-release-content.sh -p packages/react-native-navigation/datadog-mobile-react-native-navigation-v$version.tgz > packages/react-native-navigation/release-content.txt
+./check-release-content.sh -p packages/core/package.tgz > packages/core/release-content.txt
+./check-release-content.sh -p packages/react-navigation/package.tgz > packages/react-navigation/release-content.txt
+./check-release-content.sh -p packages/react-native-navigation/package.tgz > packages/react-native-navigation/release-content.txt
 
