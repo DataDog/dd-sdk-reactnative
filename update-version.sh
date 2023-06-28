3271f1e757546cc43e879e3f4e69ad1f0513e95c
louiszawadzki <louis.zawadzki@datadoghq.com>
2023-06-28 14:27:44 +0200

    Update version update script

--- update-version.sh
+++ update-version.sh
@@ -11,25 +11,19 @@ yarn workspace @datadog/mobile-react-native pack
 yarn workspace @datadog/mobile-react-navigation pack
 yarn workspace @datadog/mobile-react-native-navigation pack
 
-./check-release-content.sh -p packages/core/datadog-mobile-react-native-v$1.tgz > packages/core/release-content.txt
-./check-release-content.sh -p packages/react-navigation/datadog-mobile-react-navigation-v$1.tgz > packages/react-navigation/release-content.txt
-./check-release-content.sh -p packages/react-native-navigation/datadog-mobile-react-native-navigation-v$1.tgz > packages/react-native-navigation/release-content.txt
+./check-release-content.sh -p packages/core/package.tgz > packages/core/release-content.txt
+./check-release-content.sh -p packages/react-navigation/package.tgz > packages/react-navigation/release-content.txt
+./check-release-content.sh -p packages/react-native-navigation/package.tgz > packages/react-native-navigation/release-content.txt
 
 
 # Update example repo
 (cd example &&
-yarn remove @datadog/mobile-react-native &&
-yarn remove @datadog/mobile-react-navigation &&
-yarn remove @datadog/mobile-react-native-navigation &&
-yarn add file:../packages/core
-yarn add file:../packages/react-native-navigation
-yarn add file:../packages/react-navigation
+yarn
 (cd ios && pod install --repo-update)
 )
 
 (cd example-new-architecture &&
-yarn remove @datadog/mobile-react-native &&
-yarn add file:../packages/core
+yarn
 (cd ios && RCT_NEW_ARCH_ENABLED=1 pod install --repo-update)
 )
 
