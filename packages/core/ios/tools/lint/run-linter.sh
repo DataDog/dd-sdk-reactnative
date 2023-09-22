6fb3bd7a541b12737b29cca87a94e80a663102bc
louiszawadzki <louis.zawadzki@datadoghq.com>
2023-09-22 14:38:06 +0200

    Add linter tool for iOS

---
+++ packages/core/ios/tools/lint/run-linter.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+if [ ! -f "DdSdkReactNative.xcodeproj/project.pbxproj" ]; then
+	echo "\`run-linter.sh\` must be run in the packages/core/ios folder: \`./tools/lint/run-linter.sh\`"; exit 1
+fi
+
+automatic_fix=""
+while :; do
+    case $1 in
+        --fix) automatic_fix="--fix"            
+        ;;
+        *) break
+    esac
+    shift
+done
+
+if [[ -z "${XCODE_VERSION_ACTUAL}" ]]; then
+	# when run from command line
+	set -e # exit with error code if `swiftlint lint` fails
+	swiftlint lint --config ./tools/lint/sources.swiftlint.yml --reporter "emoji" --strict $automatic_fix
+	swiftlint lint --config ./tools/lint/tests.swiftlint.yml --reporter "emoji" --strict $automatic_fix
+else
+	# when run by Xcode in Build Phase
+	swiftlint lint --config ./tools/lint/sources.swiftlint.yml --reporter "xcode"
+	swiftlint lint --config ./tools/lint/tests.swiftlint.yml --reporter "xcode"
+fi
