3b509bb47992634d7f27261fa1965bd3ae633875
louiszawadzki <louis.zawadzki@datadoghq.com>
2023-10-26 10:16:23 +0200

    Enable Session Replay on iOS

--- packages/react-native-session-replay/ios/Sources/DdSessionReplayImplementation.swift
+++ packages/react-native-session-replay/ios/Sources/DdSessionReplayImplementation.swift
@@ -5,11 +5,56 @@
  */
 
 import Foundation
+import DatadogSessionReplay
+import DatadogInternal
 
 @objc
 public class DdSessionReplayImplementation: NSObject {
+    private lazy var sessionReplay: SessionReplayProtocol = sessionReplayProvider()
+    private let sessionReplayProvider: () -> SessionReplayProtocol
+    
+    internal init(_ sessionReplayProvider: @escaping () -> SessionReplayProtocol) {
+        self.sessionReplayProvider = sessionReplayProvider
+    }
+
+    @objc
+    public override convenience init() {
+        self.init({ NativeSessionReplay() })
+    }
+
     @objc
     public func enable(replaySampleRate: Double, defaultPrivacyLevel: String, resolve:RCTPromiseResolveBlock, reject:RCTPromiseRejectBlock) -> Void {
+        sessionReplay.enable(
+            with: SessionReplay.Configuration(
+                replaySampleRate: Float(replaySampleRate),
+                defaultPrivacyLevel: buildPrivacyLevel(privacyLevel: defaultPrivacyLevel as NSString)
+            )
+        )
         resolve(nil)
+    }
+    
+    func buildPrivacyLevel(privacyLevel: NSString) -> SessionReplay.Configuration.PrivacyLevel {
+        switch privacyLevel.lowercased {
+        case "mask":
+            return .mask
+        case "mask_user_input":
+            return .maskUserInput
+        case "allow":
+            return .allow
+        default:
+            return .mask
+        }
+    }
+}
+
+internal protocol SessionReplayProtocol {
+    func enable(
+        with configuration: SessionReplay.Configuration
+    )
+}
+
+internal class NativeSessionReplay: SessionReplayProtocol {
+    func enable(with configuration: DatadogSessionReplay.SessionReplay.Configuration) {
+        SessionReplay.enable(with: configuration)
     }
 }
