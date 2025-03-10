49f02afa22beb9ffe6e1cea29bd46fad887c90d1
Sergio Barrio <sergio.barrio@datadoghq.com>
2025-03-10 11:35:38 +0100

    Bump iOS SDK to 2.24.0

--- packages/core/ios/Tests/DdSdkTests.swift
+++ packages/core/ios/Tests/DdSdkTests.swift
@@ -434,7 +434,7 @@ class DdSdkTests: XCTestCase {
         bridge.setUser(
             user: NSDictionary(
                 dictionary: [
-                    "id": "abc-123",
+                    "id": "id_123",
                     "name": "John Doe",
                     "email": "john@doe.com",
                     "extra-info-1": 123,
@@ -449,7 +449,42 @@ class DdSdkTests: XCTestCase {
         let ddContext = try XCTUnwrap(CoreRegistry.default as? DatadogCore).contextProvider.read()
         let userInfo = try XCTUnwrap(ddContext.userInfo)
 
-        XCTAssertEqual(userInfo.id, "abc-123")
+        XCTAssertEqual(userInfo.id, "id_123")
+        XCTAssertEqual(userInfo.name, "John Doe")
+        XCTAssertEqual(userInfo.email, "john@doe.com")
+        XCTAssertEqual(userInfo.extraInfo["extra-info-1"] as? Int64, 123)
+        XCTAssertEqual(userInfo.extraInfo["extra-info-2"] as? String, "abc")
+        XCTAssertEqual(userInfo.extraInfo["extra-info-3"] as? Bool, true)
+    }
+    
+    func testSettingUserInfoOptionalId() throws {
+        let bridge = DdSdkImplementation(
+            mainDispatchQueue: DispatchQueueMock(),
+            jsDispatchQueue: DispatchQueueMock(),
+            jsRefreshRateMonitor: JSRefreshRateMonitor(),
+            RUMMonitorProvider: { MockRUMMonitor() },
+            RUMMonitorInternalProvider: { nil }
+        )
+        bridge.initialize(configuration: .mockAny(), resolve: mockResolve, reject: mockReject)
+
+        bridge.setUser(
+            user: NSDictionary(
+                dictionary: [
+                    "name": "John Doe",
+                    "email": "john@doe.com",
+                    "extra-info-1": 123,
+                    "extra-info-2": "abc",
+                    "extra-info-3": true,
+                ]
+            ),
+            resolve: mockResolve,
+            reject: mockReject
+        )
+
+        let ddContext = try XCTUnwrap(CoreRegistry.default as? DatadogCore).contextProvider.read()
+        let userInfo = try XCTUnwrap(ddContext.userInfo)
+
+        XCTAssertEqual(userInfo.id, nil)
         XCTAssertEqual(userInfo.name, "John Doe")
         XCTAssertEqual(userInfo.email, "john@doe.com")
         XCTAssertEqual(userInfo.extraInfo["extra-info-1"] as? Int64, 123)
