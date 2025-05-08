1b378af1c7c289a624117a176483f65f928bdeb1
Marco Saia <marco.saia@datadoghq.com>
2025-05-08 17:27:37 +0200

    [CHORE] Updated SDK newarch example - RN 0.79 + native unit tests

--- example-new-architecture/metro.config.js
+++ example-new-architecture/metro.config.js
@@ -14,7 +14,7 @@ const modules = Object.keys({
  * Metro configuration
  * https://facebook.github.io/metro/docs/configuration
  *
- * @type {import('metro-config').MetroConfig}
+ * @type {import('@react-native/metro-config').MetroConfig}
  */
 const config = {
   projectRoot: __dirname,
