463a58d0aa8ae6ed16100e239dd931926e9b7cb6
Sergio Barrio <sergio.barrio@datadoghq.com>
2025-09-05 17:47:29 +0200

    Use native sdk's core instance instead of the one inside RN SDK wrapper

--- packages/core/android/src/test/kotlin/com/datadog/reactnative/DdSdkTest.kt
+++ packages/core/android/src/test/kotlin/com/datadog/reactnative/DdSdkTest.kt
@@ -163,12 +163,7 @@ internal class DdSdkTest {
             answer.getArgument<Runnable>(0).run()
             true
         }
-        testedBridgeSdk = DdSdkImplementation(
-            mockReactContext,
-            mockDatadog,
-            mockDdTelemetry,
-            TestUiThreadExecutor()
-        )
+        testedBridgeSdk = DdSdkImplementation(mockReactContext, mockDatadog, mockDdTelemetry, TestUiThreadExecutor())
 
         DatadogSDKWrapperStorage.onInitializedListeners.clear()
     }
