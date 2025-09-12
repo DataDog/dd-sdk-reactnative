539de5dc7740e3baabdb30a0cd2cdef077681f4d
Sergio Barrio <sergio.barrio@datadoghq.com>
2025-09-12 11:25:08 +0200

    Update example app to add a Trace testing section

---
+++ example/src/screens/TraceScreen.tsx
@@ -0,0 +1,37 @@
+import { useState } from 'react';
+import { View, Text, Button } from 'react-native';
+
+import styles from './styles';
+import { DdTrace } from '@datadog/mobile-react-native';
+
+const TraceScreen = props => {
+    const [spanIds, setSpanIds] = useState<string[]>([]);
+
+    return (
+        <View style={styles.defaultScreen}>
+            <Text style={{ marginBottom: 20 }}>
+                Trace test
+            </Text>
+            <View style={{ marginTop: 20 }} />
+            <Button
+                title="Start Span"
+                onPress={async () => {
+                    const newSpanId = await DdTrace.startSpan("span_operation_" +   Math.floor(100 + Math.random() * 900));
+                    spanIds.push(newSpanId);
+                    setSpanIds(spanIds);
+                }}
+            />
+            <View style={{ marginTop: 20 }} />
+            <Button
+                title="Finish Span"
+                onPress={() => {
+                    const lastSpan = spanIds.pop();
+                    lastSpan && DdTrace.finishSpan(lastSpan);
+                    setSpanIds(spanIds)
+                }}
+            />
+        </View>
+    );
+};
+
+export default TraceScreen;
