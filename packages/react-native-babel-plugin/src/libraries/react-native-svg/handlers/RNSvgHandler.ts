73cc8b12677addc9efe25bf8cdb0454a6862dd86
Carlos Nogueira <carlos.nogueira@datadoghq.com>
2025-09-15 18:05:08 +0100

    Add Support for Local SVGs
    - Refactor code to allow multiple types of SVG elements

---
+++ packages/react-native-babel-plugin/src/libraries/react-native-svg/handlers/RNSvgHandler.ts
@@ -0,0 +1,259 @@
+/*
+ * Unless explicitly stated otherwise all files in this repository are licensed under the Apache License Version 2.0.
+ * This product includes software developed at Datadog (https://www.datadoghq.com/).
+ * Copyright 2016-Present Datadog, Inc.
+ */
+
+import type * as Babel from '@babel/core';
+import generate from '@babel/generator';
+import { jsxAttribute, jSXIdentifier, stringLiteral } from '@babel/types';
+
+import { getNodeName } from '../../../utils';
+import { svgSupportedNames, xmlNamespace } from '../constants';
+import {
+    buildTransformStringAttribute,
+    handleArrayAttributes,
+    handleJoinedTranformAttributes,
+    handleRegularAttributes,
+    handleRNSpecificAttributes,
+    handleSeparateTransformAttributes,
+    handleSvgDimensions,
+    validateAttribute
+} from '../processing/attributes';
+import { convertAttributeCasing } from '../utils';
+
+import type { SvgHandler } from './SvgHandler';
+
+export class RNSvgHandler implements SvgHandler {
+    constructor(
+        private types: typeof Babel.types,
+        private path: Babel.NodePath<Babel.types.JSXElement>,
+        private name: string
+    ) {
+        // no-op
+    }
+
+    /**
+     * Processes a JSXElement representing an SVG node and transforms it into
+     * a web compliant SVG string with updated attributes and dimensions.
+     * Stores the transformed SVG string and dimensions in `svgMap`, keyed by a UUID.
+     *
+     * @param dimensions - Object to collect extracted width/height info.
+     * @returns Transformed SVG JSX string, or undefined if the tag is not supported.
+     */
+    transformSvgNode(dimensions: Record<string, string>) {
+        if (!svgSupportedNames.includes(this.name)) {
+            return undefined;
+        }
+
+        const clone = this.types.cloneNode(this.path.node, true);
+
+        this.transformElement(this.types, clone, dimensions);
+        this.setNamespace(this.types, clone);
+
+        const output = generate(clone).code;
+
+        return output;
+    }
+
+    /**
+     * Sets the `xmlns` attribute on the root `<svg>` tag to ensure proper namespacing in web output.
+     *
+     * @param t - Babel types helper.
+     * @param el - JSXElement node, expected to be an `<svg>` element.
+     */
+    private setNamespace(t: typeof Babel.types, el: Babel.types.JSXElement) {
+        const name = getNodeName(t, el.openingElement.name);
+
+        if (name === 'svg') {
+            el.openingElement.attributes.push(
+                jsxAttribute(
+                    jSXIdentifier(xmlNamespace[0]),
+                    stringLiteral(xmlNamespace[1])
+                )
+            );
+        }
+    }
+
+    /**
+     * Transforms an individual JSXElement by:
+     * - Converting tag casing to be web-compatible.
+     * - Processing and sanitizing attributes.
+     * - Recursively handling children.
+     *
+     * @param t - Babel types helper.
+     * @param el - JSXElement node to transform.
+     * @param dimensions - Optional object to collect extracted width/height info.
+     */
+    private transformElement(
+        t: typeof Babel.types,
+        el: Babel.types.JSXElement,
+        dimensions: Record<string, string>
+    ) {
+        const openingNode = el.openingElement.name;
+        const isJSXIdentifierOpen = t.isJSXIdentifier(openingNode);
+
+        // Fix casing for openingElement
+        if (isJSXIdentifierOpen) {
+            openingNode.name = convertAttributeCasing(openingNode.name);
+        }
+
+        const closingNode = el.closingElement?.name;
+        const isJSXIdentifierClose = t.isJSXIdentifier(closingNode);
+
+        // Fix casing for closingElement
+        if (isJSXIdentifierClose) {
+            closingNode.name = convertAttributeCasing(closingNode.name);
+        }
+
+        this.processAttributes(t, el, dimensions);
+    }
+
+    /**
+     * Recursively traverses the children of a JSXElement and applies `transformElement`
+     * to each child that is itself a JSXElement.
+     *
+     * @param t - Babel types helper.
+     * @param jsxElement - Parent JSXElement whose children will be transformed.
+     * @param dimensions - Optional object to propagate width/height info through child elements.
+     */
+    private traverseAndTransformChildren(
+        t: typeof Babel.types,
+        jsxElement: Babel.types.JSXElement,
+        dimensions: Record<string, string> = {}
+    ) {
+        for (const child of jsxElement.children) {
+            if (t.isJSXElement(child)) {
+                this.transformElement(t, child, dimensions);
+            }
+        }
+    }
+
+    /**
+     * Processes and transforms all attributes of a given JSXElement:
+     * - Removes invalid or unsupported attributes.
+     * - Normalizes attribute casing and naming.
+     * - Consolidates transform-related attributes into a single `transform` string.
+     * - Extracts dimensions and stores them in the provided `dimensions` object.
+     * - Recursively applies transformations to child elements.
+     *
+     * @param t - Babel types helper.
+     * @param jsxElement - JSXElement whose attributes are to be processed.
+     * @param dimensions - Optional object to collect extracted width/height info.
+     */
+    private processAttributes(
+        t: typeof Babel.types,
+        jsxElement: Babel.types.JSXElement,
+        dimensions: Record<string, string> = {}
+    ) {
+        const el = jsxElement.openingElement;
+        const name = getNodeName(t, el);
+        const transformsArray: { name: string; value: string | number }[] = [];
+        const attributes = Array.from(el.attributes.entries()).reverse();
+
+        for (const [index, attr] of attributes) {
+            try {
+                if (!t.isJSXAttribute(attr)) {
+                    el.attributes.splice(index, 1);
+                    continue;
+                }
+
+                if (!t.isJSXIdentifier(attr.name)) {
+                    el.attributes.splice(index, 1);
+                    continue;
+                }
+
+                // Handle RN style attribute & non-supported attributes
+                const rnAttributesHandled = handleRNSpecificAttributes(
+                    t,
+                    attr,
+                    attr.name.name
+                );
+
+                if (rnAttributesHandled) {
+                    el.attributes.splice(index, 1);
+                    continue;
+                }
+
+                // Validate whether the attribute is valid
+                const { attrName, isInvalidAttribute } = validateAttribute(
+                    attr.name.name
+                );
+
+                if (isInvalidAttribute) {
+                    el.attributes.splice(index, 1);
+                    continue;
+                }
+
+                /* If we reach this point we know we have a valid attribute name */
+
+                // Handle SVG dimensions
+                const dimensionsHandled = handleSvgDimensions(
+                    t,
+                    attr,
+                    attrName,
+                    dimensions,
+                    name,
+                    'svg'
+                );
+
+                if (dimensionsHandled) {
+                    el.attributes.splice(index, 1);
+                    continue;
+                }
+
+                // Set the formatted attibute name to our cloned element
+                attr.name.name = attrName;
+
+                // Handle array attributes
+                const arrayAttributesHandled = handleArrayAttributes(
+                    t,
+                    attr,
+                    attrName
+                );
+
+                if (arrayAttributesHandled) {
+                    el.attributes.splice(index, 1);
+                    continue;
+                }
+
+                // Handle separate transform attributes
+                const separateTransformAttributesHandled = handleSeparateTransformAttributes(
+                    t,
+                    attr,
+                    attrName,
+                    transformsArray
+                );
+
+                if (separateTransformAttributesHandled) {
+                    el.attributes.splice(index, 1);
+                    continue;
+                }
+
+                // Handle joined transform attributes
+                const joinedTransformAttributesHandled = handleJoinedTranformAttributes(
+                    t,
+                    attr,
+                    attrName,
+                    transformsArray
+                );
+
+                if (joinedTransformAttributesHandled) {
+                    el.attributes.splice(index, 1);
+                    continue;
+                }
+
+                handleRegularAttributes(t, attr);
+            } catch (error) {
+                console.error('ReactNativeSVG[processAttributes]: ', error);
+            }
+        }
+
+        // Create & Set a new transform attribute based on the element's transform attributes
+        buildTransformStringAttribute(el, transformsArray);
+
+        // Goes through an elemnts children and transforms its properties
+        // Goes through an elements children and transforms its properties
+        this.traverseAndTransformChildren(t, jsxElement, dimensions);
+    }
+}
--- packages/react-native-babel-plugin/src/libraries/react-native-svg/index.ts
+++ packages/react-native-babel-plugin/src/libraries/react-native-svg/index.ts
@@ -5,30 +5,23 @@
  */
 
 import type * as Babel from '@babel/core';
-import generate from '@babel/generator';
+import * as parser from '@babel/parser';
+import traverse from '@babel/traverse';
 import { jsxAttribute, jSXIdentifier, stringLiteral } from '@babel/types';
+import glob from 'fast-glob';
+import fs from 'fs';
+import { default as pathN } from 'path';
 import { optimize } from 'svgo';
 import { v4 as uuidv4 } from 'uuid';
 
 import { getNodeName } from '../../utils';
 
-import { svgSupportedNames, xmlNamespace } from './constants';
-import {
-    buildTransformStringAttribute,
-    handleArrayAttributes,
-    handleJoinedTranformAttributes,
-    handleRegularAttributes,
-    handleRNSpecificAttributes,
-    handleSeparateTransformAttributes,
-    handleSvgDimensions,
-    validateAttribute
-} from './processing/attributes';
+import { HandlerResolver } from './handlers/HandlerResolver';
 import {
     clearAssetsDir,
     ensureAssetsDir,
     getAssetsPath
 } from './processing/fs';
-import { convertAttributeCasing } from './utils';
 
 type SvgOffset = {
     start: number;
@@ -42,47 +35,158 @@ export class ReactNativeSVG {
 
     svgOffset: Record<string, SvgOffset> = {};
 
-    constructor() {
+    localSvgMap: Record<string, { path: string; content?: string }> = {};
+
+    constructor(private t: typeof Babel.types, private rootDir: string) {
         this.assetsPath = getAssetsPath();
+        this.buildSvgMap();
 
         ensureAssetsDir(this.assetsPath);
         clearAssetsDir(this.assetsPath);
     }
 
     /**
-     * Processes a JSXElement representing an SVG node and transforms it into
-     * a web compliant SVG string with updated attributes and dimensions.
-     * Stores the transformed SVG string and dimensions in `svgMap`, keyed by a UUID.
+     * Scans all source files in the project to detect `.svg` imports and builds a mapping
+     * of JSX identifiers to their corresponding SVG file paths. This is done by parsing each
+     * file's AST and collecting `import` or `export` declarations that reference `.svg` files.
      *
-     * @param name - Tag name of the JSXElement (e.g., 'svg', 'path').
-     * @param path - Babel NodePath pointing to the JSXElement to process.
-     * @param t - Babel types helper.
-     * @returns Transformed SVG JSX string, or undefined if the tag is not supported.
+     * The collected mappings are stored in `localSvgMap`, keyed by the local/imported variable
+     * names (e.g., `Logo`, `IconSearch`), with their values pointing to the resolved file path.
+     *
+     * This method ignores files in `node_modules`, `lib`, and `dist`, as well as `.d.ts`, test,
+     * and config files.
      */
-    processItem(
-        name: string,
-        path: Babel.NodePath<Babel.types.JSXElement>,
-        t: typeof Babel.types
-    ) {
-        if (!svgSupportedNames.includes(name)) {
+    buildSvgMap() {
+        // IMPROVEMENT: Support aliased paths
+        const files = glob.sync(
+            ['**/*.{js,jsx,ts,tsx}', '**/*.{js,jsx,ts,tsx}'],
+            {
+                cwd: this.rootDir,
+                absolute: true,
+                ignore: [
+                    '**/node_modules/**',
+                    '**/lib/**',
+                    '**/dist/**',
+                    '**/*.d.ts',
+                    '**/*.test.*',
+                    '**/*.config.js'
+                ]
+            }
+        );
+
+        for (const file of files) {
+            try {
+                const code = fs.readFileSync(file, 'utf8');
+                if (!code) {
+                    continue;
+                }
+
+                const ast = parser.parse(code, {
+                    sourceType: 'module',
+                    plugins: [
+                        'jsx',
+                        'typescript',
+                        'exportDefaultFrom',
+                        'classProperties',
+                        'dynamicImport'
+                    ]
+                });
+
+                traverse(ast, {
+                    ImportDeclaration: path => {
+                        const source = path.node.source.value;
+                        if (!source.endsWith('.svg')) {
+                            return;
+                        }
+
+                        const resolved = pathN.resolve(
+                            pathN.dirname(file),
+                            source
+                        );
+                        for (const spec of path.node.specifiers) {
+                            const name = getNodeName(this.t, spec.local.name);
+                            if (name) {
+                                this.localSvgMap[name] = {
+                                    path: resolved
+                                };
+                            }
+                        }
+                    },
+                    ExportNamedDeclaration: path => {
+                        const source = path.node.source?.value;
+                        if (!source?.endsWith('.svg')) {
+                            return;
+                        }
+
+                        const resolved = pathN.resolve(
+                            pathN.dirname(file),
+                            source
+                        );
+                        for (const spec of path.node.specifiers) {
+                            if (spec.type === 'ExportSpecifier') {
+                                const name = getNodeName(
+                                    this.t,
+                                    spec.local.name
+                                );
+                                if (name) {
+                                    this.localSvgMap[name] = {
+                                        path: resolved
+                                    };
+                                }
+                            } else {
+                                console.warn(
+                                    `[buildSvgMap]: Unhandled export specifier type: ${spec.type}`
+                                );
+                            }
+                        }
+                    }
+                });
+            } catch (err) {
+                console.error(`[buildSvgMap]: \n File: ${file}\n`, err);
+            }
+        }
+    }
+
+    /**
+     * Processes a JSXElement representing an SVG-based component and transforms it into
+     * a web-compliant SVG string with normalized attributes and extracted dimensions.
+     * The resulting SVG content and its metadata (e.g., width/height) are stored in `svgMap`,
+     * keyed by a generated UUID for later reference.
+     *
+     * Internally, the appropriate handler is selected based on the tag name and used to
+     * perform the transformation.
+     *
+     * @param path - Babel NodePath pointing to the JSXElement to process.
+     * @param name - JSX tag name (e.g., 'Svg', 'Logo') used to resolve the appropriate handler.
+     * @returns An object containing the original SVG string and its optimized version,
+     *          or `undefined` if no transformation could be performed.
+     */
+    processItem(path: Babel.NodePath<Babel.types.JSXElement>, name: string) {
+        const dimensions: Record<string, string> = {};
+
+        HandlerResolver.configure({
+            t: this.t,
+            path,
+            name,
+            localSvgMap: this.localSvgMap
+        });
+
+        const handler = HandlerResolver.create();
+        const output = handler?.transformSvgNode(dimensions);
+
+        if (!output) {
             return;
         }
 
-        const clone = t.cloneNode(path.node, true);
-        const dimensions: Record<string, string> = {};
         const id = uuidv4();
-
-        this.transformElement(clone, dimensions, t);
-        this.setNamespace(t, clone);
-
-        const output = generate(clone).code;
+        const originalNode = path.node;
 
         this.svgMap[id] = {
             file: output,
             ...dimensions
         };
 
-        this.setNativeID(path.node, id);
+        this.setNativeID(originalNode, id);
 
         const optimized = optimize(output, {
             multipass: true,
@@ -90,25 +194,6 @@ export class ReactNativeSVG {
         });
 
         return { original: output, optimized };
-    }
-
-    /**
-     * Sets the `xmlns` attribute on the root `<svg>` tag to ensure proper namespacing in web output.
-     *
-     * @param t - Babel types helper.
-     * @param el - JSXElement node, expected to be an `<svg>` element.
-     */
-    private setNamespace(t: typeof Babel.types, el: Babel.types.JSXElement) {
-        const name = getNodeName(t, el.openingElement.name);
-
-        if (name === 'svg') {
-            el.openingElement.attributes.push(
-                jsxAttribute(
-                    jSXIdentifier(xmlNamespace[0]),
-                    stringLiteral(xmlNamespace[1])
-                )
-            );
-        }
     }
 
     /**
@@ -122,186 +207,5 @@ export class ReactNativeSVG {
         el.openingElement.attributes.push(
             jsxAttribute(jSXIdentifier('nativeID'), stringLiteral(id))
         );
-    }
-
-    /**
-     * Transforms an individual JSXElement by:
-     * - Converting tag casing to be web-compatible.
-     * - Processing and sanitizing attributes.
-     * - Recursively handling children.
-     *
-     * @param el - JSXElement node to transform.
-     * @param dimensions - Optional object to collect extracted width/height info.
-     * @param t - Babel types helper.
-     */
-    private transformElement(
-        el: Babel.types.JSXElement,
-        dimensions: Record<string, string> = {},
-        t: typeof Babel.types
-    ) {
-        const openingNode = el.openingElement.name;
-        const isJSXIdentifierOpen = t.isJSXIdentifier(openingNode);
-
-        // Fix casing for openingElement
-        if (isJSXIdentifierOpen) {
-            openingNode.name = convertAttributeCasing(openingNode.name);
-        }
-
-        const closingNode = el.closingElement?.name;
-        const isJSXIdentifierClose = t.isJSXIdentifier(closingNode);
-
-        // Fix casing for closingElement
-        if (isJSXIdentifierClose) {
-            closingNode.name = convertAttributeCasing(closingNode.name);
-        }
-
-        this.processAttributes(el, dimensions, t);
-    }
-
-    /**
-     * Recursively traverses the children of a JSXElement and applies `transformElement`
-     * to each child that is itself a JSXElement.
-     *
-     * @param t - Babel types helper.
-     * @param jsxElement - Parent JSXElement whose children will be transformed.
-     * @param dimensions - Optional object to propagate width/height info through child elements.
-     */
-    private traverseAndTransformChildren(
-        t: typeof Babel.types,
-        jsxElement: Babel.types.JSXElement,
-        dimensions: Record<string, string> = {}
-    ) {
-        for (const child of jsxElement.children) {
-            if (t.isJSXElement(child)) {
-                this.transformElement(child, dimensions, t);
-            }
-        }
-    }
-
-    /**
-     * Processes and transforms all attributes of a given JSXElement:
-     * - Removes invalid or unsupported attributes.
-     * - Normalizes attribute casing and naming.
-     * - Consolidates transform-related attributes into a single `transform` string.
-     * - Extracts dimensions and stores them in the provided `dimensions` object.
-     * - Recursively applies transformations to child elements.
-     *
-     * @param jsxElement - JSXElement whose attributes are to be processed.
-     * @param dimensions - Optional object to collect extracted width/height info.
-     * @param t - Babel types helper.
-     */
-    private processAttributes(
-        jsxElement: Babel.types.JSXElement,
-        dimensions: Record<string, string> = {},
-        t: typeof Babel.types
-    ) {
-        const el = jsxElement.openingElement;
-        const name = getNodeName(t, el);
-        const transformsArray: { name: string; value: string | number }[] = [];
-        const attributes = Array.from(el.attributes.entries()).reverse();
-
-        for (const [index, attr] of attributes) {
-            try {
-                if (!t.isJSXAttribute(attr)) {
-                    el.attributes.splice(index, 1);
-                    continue;
-                }
-
-                if (!t.isJSXIdentifier(attr.name)) {
-                    el.attributes.splice(index, 1);
-                    continue;
-                }
-
-                // Handle RN style attribute & non-supported attributes
-                const rnAttributesHandled = handleRNSpecificAttributes(
-                    t,
-                    attr,
-                    attr.name.name
-                );
-
-                if (rnAttributesHandled) {
-                    el.attributes.splice(index, 1);
-                    continue;
-                }
-
-                // Validate whether the attribute is valid
-                const { attrName, isInvalidAttribute } = validateAttribute(
-                    attr.name.name
-                );
-
-                if (isInvalidAttribute) {
-                    el.attributes.splice(index, 1);
-                    continue;
-                }
-
-                /* If we reach this point we know we have a valid attribute name */
-
-                // Handle SVG dimensions
-                const dimensionsHandled = handleSvgDimensions(
-                    t,
-                    attr,
-                    attrName,
-                    dimensions,
-                    name
-                );
-
-                if (dimensionsHandled) {
-                    el.attributes.splice(index, 1);
-                    continue;
-                }
-
-                // Set the formatted attibute name to our cloned element
-                attr.name.name = attrName;
-
-                // Handle array attributes
-                const arrayAttributesHandled = handleArrayAttributes(
-                    t,
-                    attr,
-                    attrName
-                );
-
-                if (arrayAttributesHandled) {
-                    el.attributes.splice(index, 1);
-                    continue;
-                }
-
-                // Handle separate transform attributes
-                const separateTransformAttributesHandled = handleSeparateTransformAttributes(
-                    t,
-                    attr,
-                    attrName,
-                    transformsArray
-                );
-
-                if (separateTransformAttributesHandled) {
-                    el.attributes.splice(index, 1);
-                    continue;
-                }
-
-                // Handle joined transform attributes
-                const joinedTransformAttributesHandled = handleJoinedTranformAttributes(
-                    t,
-                    attr,
-                    attrName,
-                    transformsArray
-                );
-
-                if (joinedTransformAttributesHandled) {
-                    el.attributes.splice(index, 1);
-                    continue;
-                }
-
-                handleRegularAttributes(t, attr);
-            } catch (error) {
-                console.error('ReactNativeSVG[processAttributes]: ', error);
-            }
-        }
-
-        // Create & Set a new transform attribute based on the element's transform attributes
-        buildTransformStringAttribute(el, transformsArray);
-
-        // Goes through an elemnts children and transforms its properties
-        // Goes through an elements children and transforms its properties
-        this.traverseAndTransformChildren(t, jsxElement, dimensions);
     }
 }
