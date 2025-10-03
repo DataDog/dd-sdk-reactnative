e5183f52fc50de34351094818c693e0a3790e9c7
Carlos Nogueira <carlos.nogueira@datadoghq.com>
2025-10-03 17:05:03 +0100

    Add metro plugin to session replay package

---
+++ packages/react-native-session-replay/src/metro/index.ts
@@ -0,0 +1,75 @@
+/*
+ * Unless explicitly stated otherwise all files in this repository are licensed under the Apache License Version 2.0.
+ * This product includes software developed at Datadog (https://www.datadoghq.com/).
+ * Copyright 2016-Present Datadog, Inc.
+ */
+
+import chokidar from 'chokidar';
+import fs from 'fs';
+import path from 'path';
+
+import { mergeSvgAssets } from './processing';
+import { debounce } from './utils';
+
+let watching = false;
+
+export function withSessionReplayAggregator(metroConfig: any): any {
+    const originalReporter = metroConfig.reporter;
+
+    const assetsDir = path.resolve(__dirname, '../../../assets');
+
+    if (!fs.existsSync(assetsDir)) {
+        fs.mkdirSync(assetsDir, { recursive: true });
+    }
+
+    const debounceMerge = debounce(() => {
+        try {
+            mergeSvgAssets(assetsDir);
+        } catch (error) {
+            console.warn('[SessionReplayAggregator] merge failed:', error);
+        }
+    }, 300);
+
+    // Prevent potential multiple watchers if metro loads the config multiple times
+    if (!watching) {
+        watching = true;
+        chokidar
+            .watch(assetsDir, {
+                // Skip events for files that already exist
+                ignoreInitial: true,
+                depth: 0,
+                awaitWriteFinish: {
+                    // Wait 200ms after last change
+                    stabilityThreshold: 200,
+                    // Check every 50ms
+                    pollInterval: 50
+                }
+            })
+            .on('add', file => {
+                if (!file.endsWith('.svg')) {
+                    return;
+                }
+
+                debounceMerge();
+            });
+    }
+
+    return {
+        ...metroConfig,
+        reporter: {
+            update(event: any) {
+                if (originalReporter?.update) {
+                    originalReporter.update(event);
+                }
+
+                // https://github.com/facebook/metro/blob/main/packages/metro/src/lib/reporting.js
+                if (
+                    event.type === 'bundle_build_done' ||
+                    event.type === 'transformer_load_done'
+                ) {
+                    mergeSvgAssets(assetsDir);
+                }
+            }
+        }
+    };
+}
---
+++ packages/react-native-session-replay/src/metro/processing.ts
@@ -0,0 +1,99 @@
+/*
+ * Unless explicitly stated otherwise all files in this repository are licensed under the Apache License Version 2.0.
+ * This product includes software developed at Datadog (https://www.datadoghq.com/).
+ * Copyright 2016-Present Datadog, Inc.
+ */
+
+import fs from 'fs';
+import path from 'path';
+
+type SvgIndexEntry = {
+    offset: number;
+    length: number;
+};
+
+type SvgIndex = Record<string, SvgIndexEntry>;
+
+export function mergeSvgAssets(assetsDir: string) {
+    // const assetsDir = 'node_modules/@datadog/mobile-react-native-session-replay/assets'
+    const binName = 'assets.bin';
+    const jsonName = 'assets.json';
+
+    const binPath = path.resolve(assetsDir, binName);
+    const jsonPath = path.resolve(assetsDir, jsonName);
+
+    let index: SvgIndex = {};
+    let offset = 0;
+
+    if (!fs.existsSync(assetsDir)) {
+        fs.mkdirSync(assetsDir, { recursive: true });
+    }
+
+    if (fs.existsSync(binPath) && fs.existsSync(jsonPath)) {
+        try {
+            index = JSON.parse(fs.readFileSync(jsonPath, 'utf8')) as SvgIndex;
+            offset = fs.statSync(binPath).size;
+        } catch (err) {
+            console.warn(
+                '[mergeSvgAssets] Failed to read index, starting fresh:',
+                err
+            );
+
+            index = {};
+            offset = 0;
+
+            if (fs.existsSync(binPath)) {
+                fs.unlinkSync(binPath);
+            }
+            if (fs.existsSync(jsonPath)) {
+                fs.unlinkSync(jsonPath);
+            }
+        }
+    } else {
+        if (fs.existsSync(binPath)) {
+            fs.unlinkSync(binPath);
+        }
+        if (fs.existsSync(jsonPath)) {
+            fs.unlinkSync(jsonPath);
+        }
+    }
+
+    const files = fs.readdirSync(assetsDir).sort();
+    let added = 0;
+
+    for (const f of files) {
+        if (f === binName || f === jsonName) {
+            continue;
+        }
+
+        const id = path.basename(f, path.extname(f));
+        if (index[id]) {
+            continue;
+        }
+
+        try {
+            const svg = fs.readFileSync(path.join(assetsDir, f), 'utf8');
+            const buf = Buffer.from(svg, 'utf8');
+            const length = buf.length;
+
+            fs.appendFileSync(binPath, buf);
+            index[id] = { offset, length };
+            offset += length;
+            added++;
+        } catch (err) {
+            console.warn(
+                `[SessionReplayAggregator] Failed to process ${f}:`,
+                err
+            );
+        }
+    }
+
+    fs.writeFileSync(jsonPath, JSON.stringify(index, null, 2));
+    if (added > 0) {
+        console.log(
+            `[SessionReplayAggregator] Packed ${added} new Session Replay SVG assets → total: ${
+                Object.keys(index).length
+            }`
+        );
+    }
+}
