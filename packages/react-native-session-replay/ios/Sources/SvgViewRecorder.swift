de4d5d4ed2ada93613382225e39a86ba007a9c47
Carlos Nogueira <carlos.nogueira@datadoghq.com>
2025-10-21 18:36:53 +0100

    Improve SvgViewRecorder on iOS
    - Only do data checks if the view in question is our custom
    SesssionReplayView
    - Remove accessibilityIdentifier check as it's not needed
    - Update file reading logic to support <iOS 13

--- packages/react-native-session-replay/ios/Sources/SvgViewRecorder.swift
+++ packages/react-native-session-replay/ios/Sources/SvgViewRecorder.swift
@@ -12,7 +12,6 @@ import DatadogSDKReactNative
 import UIKit
 import React
 
-
 private enum SVGConstants {
     static let attributes = "attributes"
     static let width = "width"
@@ -20,6 +19,11 @@ private enum SVGConstants {
     static let hash = "hash"
     static let type = "type"
     static let svgTypeValue = "svg"
+}
+
+private enum PrivacyViewClasses {
+    static let paper = "DdPrivacyView"
+    static let fabric = "DdPrivacyViewFabric"
 }
 
 internal struct ReactNativeSVGResource: SessionReplayResource {
@@ -41,7 +45,6 @@ internal struct ReactNativeSVGResource: SessionReplayResource {
     }
 }
 
-
 internal class SvgViewRecorder: SessionReplayNodeRecorder {
     internal var identifier = UUID()
     
@@ -60,15 +63,18 @@ internal class SvgViewRecorder: SessionReplayNodeRecorder {
         with attributes: SessionReplayViewAttributes,
         in context: SessionReplayViewTreeRecordingContext
     ) -> SessionReplayNodeSemantics? {
-        
-        guard view.accessibilityIdentifier != nil else {
-            return nil
+
+        if let cls1 = NSClassFromString(PrivacyViewClasses.paper),
+           let cls2 = NSClassFromString(PrivacyViewClasses.fabric) {
+            if !view.isKind(of: cls1) && !view.isKind(of: cls2) {
+                return nil
+            }
         }
-        
+
         guard let attrs = view.value(forKey: SVGConstants.attributes) as? [String: String] else {
             return nil
         }
-        
+
         guard let type = attrs[SVGConstants.type], type == "svg" else {
             return nil
         }
@@ -89,56 +95,61 @@ internal class SvgViewRecorder: SessionReplayNodeRecorder {
         let viewId = context.ids.nodeID(view: view, nodeRecorder: self)
         let svgId = context.ids.nodeID(view: subView, nodeRecorder: self)
         
-        do {
-            guard let svgInfo = svgMap[hash] else {
-                return nil
-            }
-            
-            let fileHandle = try FileHandle(forReadingFrom: url)
-            defer { try? fileHandle.close() }
-            
-            try fileHandle.seek(toOffset: UInt64(svgInfo.offset))
-            let svgDataChunk = try fileHandle.read(upToCount: svgInfo.length)
-            
-            guard let svgDataChunk = svgDataChunk,
-                  var svgData = String(data: svgDataChunk, encoding: .utf8) else {
-                return nil
-            }
-            
-            if let updatedSvgData = handleDynamicSvgDimensions(view: subView, svgData: svgData, attrs: attrs) {
-                svgData = updatedSvgData
-            }
-            
-            let svgResource = ReactNativeSVGResource(
-                id: hash,
-                svgContent: svgData
-            )
-            
-            let contentFrame = CGRect(
-                origin: attributes.frame.origin,
-                size: CGSize(width: subView.bounds.width,
-                             height: subView.bounds.height)
-            )
-            
-            let builder = SvgViewWireframesBuilder(
-                wireframeID: viewId,
-                imageWireframeID: svgId,
-                attributes: attributes,
-                contentFrame: contentFrame,
-                svgResource: svgResource,
-                imagePrivacyLevel: context.recorder.imagePrivacy
-            )
-            
-            // Children are ignored because they were already processed and converted by the babel plugin
-            let element = SessionReplaySpecificElement(subtreeStrategy: .ignore, nodes: [
-                SessionReplayNode(viewAttributes: attributes, wireframesBuilder: builder)
-            ])
-            
-            return element
-        } catch {
+        guard let svgInfo = svgMap[hash] else {
             return nil
         }
+        
+        guard let svgDataChunk = readDataChunk(from: url, offset: svgInfo.offset, length: svgInfo.length),
+              var svgData = String(data: svgDataChunk, encoding: .utf8) else {
+            return nil
+        }
+        
+        
+        if let updatedSvgData = handleDynamicSvgDimensions(view: subView, svgData: svgData, attrs: attrs) {
+            svgData = updatedSvgData
+        }
+        
+        let svgResource = ReactNativeSVGResource(
+            id: hash,
+            svgContent: svgData
+        )
+        
+        let contentFrame = CGRect(
+            origin: attributes.frame.origin,
+            size: CGSize(width: subView.bounds.width,
+                         height: subView.bounds.height)
+        )
+        
+        let builder = SvgViewWireframesBuilder(
+            wireframeID: viewId,
+            imageWireframeID: svgId,
+            attributes: attributes,
+            contentFrame: contentFrame,
+            svgResource: svgResource,
+            imagePrivacyLevel: context.recorder.imagePrivacy
+        )
+        
+        // Children are ignored because they were already processed and converted by the babel plugin
+        let element = SessionReplaySpecificElement(subtreeStrategy: .ignore, nodes: [
+            SessionReplayNode(viewAttributes: attributes, wireframesBuilder: builder)
+        ])
+        
+        return element
     }
+}
+
+func readDataChunk(from url: URL, offset: Int, length: Int) -> Data? {
+    guard let fileHandle = FileHandle(forReadingAtPath: url.path) else {
+        return nil
+    }
+    defer {
+        fileHandle.closeFile()
+    }
+    
+    fileHandle.seek(toFileOffset: UInt64(offset))
+    
+    let data = fileHandle.readData(ofLength: length)
+    return data
 }
 
 func handleDynamicSvgDimensions(view: UIView, svgData: String, attrs: [String: String]) -> String? {
@@ -174,7 +185,6 @@ func handleDynamicSvgDimensions(view: UIView, svgData: String, attrs: [String: S
     
     return nil
 }
-
 
 internal struct SvgViewWireframesBuilder: SessionReplayNodeWireframesBuilder {
     let wireframeID: WireframeID
