2bedb0d2a87b714f7d27228f706d3e6360c2f811
Carlos Nogueira <carlos.nogueira@datadoghq.com>
2025-11-03 18:05:04 +0000

    Expose `setAccountInfo` API to JS layer
    add account info to `applyEventMapper`

---
+++ packages/core/src/sdk/AccountInfoSingleton/AccountInfoSingleton.ts
@@ -0,0 +1,46 @@
+/*
+ * Unless explicitly stated otherwise all files in this repository are licensed under the Apache License Version 2.0.
+ * This product includes software developed at Datadog (https://www.datadoghq.com/).
+ * Copyright 2016-Present Datadog, Inc.
+ */
+
+import type { AccountInfo } from './types';
+
+class AccountInfoProvider {
+    private accountInfo: AccountInfo | undefined = undefined;
+
+    setAccountInfo = (accountInfo: AccountInfo) => {
+        this.accountInfo = accountInfo;
+    };
+
+    addAccountExtraInfo = (extraInfo: AccountInfo['extraInfo']) => {
+        if (!this.accountInfo) {
+            return;
+        }
+
+        this.accountInfo.extraInfo = {
+            ...this.accountInfo.extraInfo,
+            ...extraInfo
+        };
+    };
+
+    getAccountInfo = (): AccountInfo | undefined => {
+        return this.accountInfo;
+    };
+
+    clearAccountInfo = () => {
+        this.accountInfo = undefined;
+    };
+}
+
+export class AccountInfoSingleton {
+    private static accountInfoProvider = new AccountInfoProvider();
+
+    static getInstance = (): AccountInfoProvider => {
+        return AccountInfoSingleton.accountInfoProvider;
+    };
+
+    static reset = () => {
+        AccountInfoSingleton.accountInfoProvider = new AccountInfoProvider();
+    };
+}
