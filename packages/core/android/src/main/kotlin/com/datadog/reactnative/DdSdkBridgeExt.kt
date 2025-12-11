8b787317ff5ae921b0ed7544be730d82fbae06db
Dmytrii Puzyr <dmytrii.puzyr@datadoghq.com>
2025-12-11 20:08:57 +0200

    Move extension methods to the general file, add tests for them

--- packages/core/android/src/main/kotlin/com/datadog/reactnative/DdSdkBridgeExt.kt
+++ packages/core/android/src/main/kotlin/com/datadog/reactnative/DdSdkBridgeExt.kt
@@ -242,3 +242,99 @@ internal fun ReadableMap.getDoubleOrNull(key: String): Double? {
         null
     }
 }
+
+/**
+ * Converts a [JSONObject] to a [WritableMap].
+ */
+internal fun JSONObject.toWritableMap(): WritableMap = this.toMap().toWritableMap()
+
+/**
+ * Converts a [JSONObject] to a [Map].
+ */
+internal fun JSONObject.toMap(): Map<String, Any?> {
+    val map = mutableMapOf<String, Any?>()
+    val keys = this.keys()
+
+    while (keys.hasNext()) {
+        val key = keys.next()
+        val value = this.opt(key)
+
+        map[key] =
+            when (value) {
+                null, JSONObject.NULL -> null
+                is JSONObject -> value.toMap()
+                is org.json.JSONArray -> value.toList()
+                else -> value
+            }
+    }
+
+    return map
+}
+
+/**
+ * Converts a [org.json.JSONArray] to a [List].
+ */
+internal fun org.json.JSONArray.toList(): List<Any?> {
+    val list = mutableListOf<Any?>()
+
+    for (i in 0 until this.length()) {
+        val value = this.opt(i)
+
+        list.add(
+            when (value) {
+                null, JSONObject.NULL -> null
+                is JSONObject -> value.toMap()
+                is org.json.JSONArray -> value.toList()
+                else -> value
+            },
+        )
+    }
+
+    return list
+}
+
+/**
+ * Converts a [ReadableMap] to a [JSONObject].
+ */
+internal fun ReadableMap.toJSONObject(): JSONObject = this.toMap().toJSONObject()
+
+/**
+ * Converts a [Map] to a [JSONObject].
+ */
+@Suppress("UNCHECKED_CAST")
+internal fun Map<String, Any>.toJSONObject(): JSONObject {
+    val jsonObject = JSONObject()
+
+    for ((key, value) in this) {
+        jsonObject.put(
+            key,
+            when (value) {
+                is Map<*, *> -> (value as Map<String, Any>).toJSONObject()
+                is List<*> -> value.toJSONArray()
+                else -> value
+            },
+        )
+    }
+
+    return jsonObject
+}
+
+/**
+ * Converts a [List] to a [org.json.JSONArray].
+ */
+@Suppress("UNCHECKED_CAST")
+internal fun List<*>.toJSONArray(): org.json.JSONArray {
+    val jsonArray = org.json.JSONArray()
+
+    for (value in this) {
+        jsonArray.put(
+            when (value) {
+                is Map<*, *> -> (value as Map<String, Any>).toJSONObject()
+                is List<*> -> value.toJSONArray()
+                else -> value
+            },
+        )
+    }
+
+    return jsonArray
+}
