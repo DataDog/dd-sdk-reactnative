731d0a882c176b926a93f547c7c7f92e4dc7fafd
Marco Saia <marco.saia@datadoghq.com>
2026-01-05 08:29:32 +0100

    review: using parsed RN version in test snapshot

--- packages/core/src/sdk/DatadogProvider/__tests__/initialization.test.tsx
+++ packages/core/src/sdk/DatadogProvider/__tests__/initialization.test.tsx
@@ -4,6 +4,7 @@
  * Copyright 2016-Present Datadog, Inc.
  */
 
+import { version as reactNativeVersion } from 'react-native/package.json';
 import { NativeModules } from 'react-native';
 
 import { InitializationMode } from '../../../config/types';
@@ -64,7 +65,7 @@ describe('DatadogProvider', () => {
             expect(receivedConfiguration).toMatchInlineSnapshot(`
                 DdSdkNativeConfiguration {
                   "additionalConfiguration": {
-                    "_dd.react_native_version": "0.76.9",
+                    "_dd.react_native_version": "${reactNativeVersion}",
                     "_dd.source": "react-native",
                   },
                   "attributeEncoders": [],
