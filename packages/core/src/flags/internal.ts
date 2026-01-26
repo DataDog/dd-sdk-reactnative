492631c0bb11daee6ee3f71b6c92c046cc17bb18
Dmytrii Puzyr <dmytrii.puzyr@datadoghq.com>
2026-01-26 12:13:49 +0200

    Fix imports

--- packages/core/src/flags/internal.ts
+++ packages/core/src/flags/internal.ts
@@ -1,5 +1,5 @@
 import { InternalLog } from '../InternalLog';
-import { SdkVerbosity } from '../SdkVerbosity';
+import { SdkVerbosity } from '../config/types/SdkVerbosity';
 
 import type { EvaluationContext, FlagDetails } from './types';
 
